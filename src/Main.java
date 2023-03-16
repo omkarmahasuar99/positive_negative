bf6e25ec76e106ac20117496bbeeda21ac810910
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-16 18:16:22 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Negative_or_Positive
+{
+    public static void main(String[] args)
+    {
+        int a = -89;
+        if (a>0)
+            System.out.println(a+" is a positive number");
+        else System.out.println(a+" is a negative number");
+
+    }
+}
